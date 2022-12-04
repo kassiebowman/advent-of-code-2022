87237f1f45cb8ac49f8ec88e50dd00d183c19cdc
Kassie Bowman <19256422+kassiebowman@users.noreply.github.com>
2022-12-03 21:53:06 -0800

    Cleanup day 4

--- src/main/groovy/aoc/Day04.groovy
+++ src/main/groovy/aoc/Day04.groovy
@@ -6,35 +6,39 @@ package aoc
  * @see <ahref="https://adventofcode.com/2022/day/4">AOC 2022 Day 4</a>
  */
 class Day04 {
+    /**
+     * Determine how many pairs of section assignments exist where one assignment fully contains the other.
+     *
+     * @param fileName The name of the file containing the pairs of section assignments, formatted as ##-##,##-##
+     * @return The number of section assignment pairs where one pair fully contains the other.
+     */
     int findFullyContainedPairs(String fileName) {
         def fullyContainedPairs = 0
-        getClass().getClassLoader().getResource(fileName).splitEachLine(",", pairs -> {
-            def section1 = pairs[0].split("-")
-            def range1 = ((section1[0] as int)..(section1[1] as int))
-            def section2 = pairs[1].split("-")
-            def range2 = ((section2[0] as int)..(section2[1] as int))
-            def isContained = range1.containsAll(range2) || range2.containsAll(range1)
-            fullyContainedPairs += isContained ? 1 : 0
-
-//            println "Range1: $range1, range2: $range2, contained: $isContained"
+        getClass().getClassLoader().getResource(fileName).splitEachLine("[,-]", pairs -> {
+            def range1 = ((pairs[0] as int)..(pairs[1] as int))
+            def range2 = ((pairs[2] as int)..(pairs[3] as int))
+            if(range1.containsAll(range2) || range2.containsAll(range1)) fullyContainedPairs++
+//            println "range1: $range1, range2: $range2, fullyContainedPairs: $fullyContainedPairs"
         })
 
-        return fullyContainedPairs;
+        return fullyContainedPairs
     }
 
+    /**
+     * Determine how many pairs of section assignments exist where one assignment overlaps the other.
+     *
+     * @param fileName The name of the file containing the pairs of section assignments, formatted as ##-##,##-##
+     * @return The number of section assignment pairs where one pair overlaps the other.
+     */
     int findOverlappingPairs(String fileName) {
         def overlappingPairs = 0
-        getClass().getClassLoader().getResource(fileName).splitEachLine(",", pairs -> {
-            def section1 = pairs[0].split("-")
-            def range1 = ((section1[0] as int)..(section1[1] as int))
-            def section2 = pairs[1].split("-")
-            def range2 = ((section2[0] as int)..(section2[1] as int))
-            def overlaps = !range1.intersect(range2).isEmpty()
-            overlappingPairs += overlaps ? 1 : 0
-
-//            println "Range1: $range1, range2: $range2, overlaps: $overlaps"
+        getClass().getClassLoader().getResource(fileName).splitEachLine("[,-]", pairs -> {
+            def range1 = ((pairs[0] as int)..(pairs[1] as int))
+            def range2 = ((pairs[2] as int)..(pairs[3] as int))
+            if (!range1.intersect(range2).isEmpty()) overlappingPairs++
+//            println "range1: $range1, range2: $range2, overlappingPairs: $overlappingPairs"
         })
 
-        return overlappingPairs;
+        return overlappingPairs
     }
 }
