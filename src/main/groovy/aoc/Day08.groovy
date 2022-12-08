cd1b8fdf01877782b9adc4217a3c3048412f52ce
Kassie Bowman <19256422+kassiebowman@users.noreply.github.com>
2022-12-07 22:41:05 -0800

    Clean up and document day 8

--- src/main/groovy/aoc/Day08.groovy
+++ src/main/groovy/aoc/Day08.groovy
@@ -2,12 +2,19 @@ package aoc
 /**
  * Day 8: Treetop Tree House
  *
- * @see <ahref="https://adventofcode.com/2022/day/8" > AOC 2022 Day 8</a>
+ * @see <ahref="https://adventofcode.com/2022/day/8"  >  AOC 2022 Day 8</a>
  */
 class Day08 {
-
-    int findVisibleTrees(String fileName, boolean part1) {
-
+    /**
+     * Checks the visibility of trees to determine which trees are visible outside the forest or which have the farthest
+     * sight lines within the forest.
+     *
+     * @param fileName The name of the file containing the tree heights, as a list of strings
+     * @param part1 Whether to return the number of visible trees of the highest scenic score
+     * @return If part1 is {@code true}, returns the number of visible trees. If part1 is {@code false}, returns the
+     * highest scenic score of any tree.
+     */
+    int checkTreeVisibility(String fileName, boolean part1) {
         def treeRows = getClass().getClassLoader().getResource(fileName).readLines()
         def numColumns = treeRows.size()
         def numRows = treeRows[0].size()
@@ -22,6 +29,7 @@ class Day08 {
                 def indexDown = getIndexOfFirstTallTreeInColumn(r, c, treeRows, false)
 
                 if (indexLeft == -1 || indexRight == -1 || indexUp == -1 || indexDown == -1) visibleInnerTrees++
+
                 def scenicScore = (indexUp == -1 ? r : r - indexUp) *
                         (indexLeft == -1 ? c : c - indexLeft) *
                         (indexRight == -1 ? numColumns - 1 - c : indexRight - c) *
@@ -35,18 +43,22 @@ class Day08 {
                 maxScenicScore
     }
 
-    boolean isHiddenLeftAndRight(int rowIndex, int columnIndex, List<String> treeRows) {
-        def hiddenLeft = getIndexOfFirstTallTreeInRow(rowIndex, columnIndex, treeRows, true) != -1;
-        if (!hiddenLeft) return false
-
-        return getIndexOfFirstTallTreeInRow(rowIndex, columnIndex, treeRows, false) != -1;
-    }
-
+    /**
+     * Gets the column index of the first tree in the row with a height greater than or equal to the reference tree at
+     * the specified row and column indices.
+     *
+     * @param rowIndex The row index of the reference tree
+     * @param columnIndex The column index of the reference tree
+     * @param treeRows List of tree rows
+     * @param left {@code true} if looking for a tall tree to the left; {@code false} to look for a tall tree to the right
+     * @return The column index of the first tree with height greater than or equal to the reference tree in the
+     * specified direction (left or right), or -1 if no trees are of greater or equal height
+     */
     int getIndexOfFirstTallTreeInRow(int rowIndex, int columnIndex, List<String> treeRows, boolean left) {
         def treeRow = treeRows[rowIndex]
         def tree = treeRow[columnIndex] as int
 
-        def columnIndices = left ? (0..<columnIndex).reverse() : ((columnIndex + 1)..<treeRow.size())
+        def columnIndices = left ? (columnIndex - 1)..0 : ((columnIndex + 1)..<treeRow.size())
         for (i in columnIndices) {
             if (treeRow[i] as int >= tree) {
                 return i
@@ -56,6 +68,17 @@ class Day08 {
         return -1
     }
 
+    /**
+     * Gets the row index of the first tree in the column with a height greater than or equal to the reference tree at
+     * the specified row and column indices.
+     *
+     * @param rowIndex The row index of the reference tree
+     * @param columnIndex The column index of the reference tree
+     * @param treeRows List of tree rows
+     * @param up {@code true} if looking for a tall tree above the reference tree; {@code false} to look for a tall tree below
+     * @return The row index of the first tree with height greater than or equal to the reference tree in the
+     * specified direction (up or down), or -1 if no trees are of greater or equal height
+     */
     int getIndexOfFirstTallTreeInColumn(int rowIndex, int columnIndex, List<String> treeRows, boolean up) {
         def tree = treeRows[rowIndex][columnIndex] as int
 
--- src/test/groovy/aoc/Day08Test.groovy
+++ src/test/groovy/aoc/Day08Test.groovy
@@ -16,7 +16,7 @@ class Day08Test {
             "08-control.txt,8,false",
             "08-data.txt,671160,false",
     ])
-    void findVisibleTrees(String fileName, int result, boolean part1) {
-        assertThat(new Day08().findVisibleTrees(fileName, part1)).isEqualTo(result)
+    void checkTreeVisibility(String fileName, int result, boolean part1) {
+        assertThat(new Day08().checkTreeVisibility(fileName, part1)).isEqualTo(result)
     }
 }
