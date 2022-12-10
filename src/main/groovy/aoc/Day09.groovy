6db5f64675ed89defc526ad824b47c007387fecf
Kassie Bowman <19256422+kassiebowman@users.noreply.github.com>
2022-12-09 22:54:04 -0800

    Solve day 9, part 2

--- src/main/groovy/aoc/Day09.groovy
+++ src/main/groovy/aoc/Day09.groovy
@@ -2,57 +2,75 @@ package aoc
 /**
  * Day 9: Rope Bridge
  *
- * @see <ahref="https://adventofcode.com/2022/day/9">AOC 2022 Day 9</a>
+ * @see <ahref="https://adventofcode.com/2022/day/9"    >    AOC 2022 Day 9</a>
  */
 class Day09 {
     int countVisitedCoordinates(String fileName, boolean part1) {
         Set<XY> visitedCoords = []
-        visitedCoords << new XY(0,0)
 
-        def headX = 0
-        def headY = 0
-        def tailX = 0
-        def tailY = 0
+        List<XY> knots = []
+        int numKnots = part1 ? 2 : 10
+        for (i in 0..<numKnots) {
+            knots << new XY(0, 0)
+        }
+
+        visitedCoords << new XY(0, 0)
+
         getClass().getClassLoader().getResource(fileName).splitEachLine(" ", params -> {
             def direction = params[0]
             def numSteps = params[1] as int
 
             def offsetX = 0
             def offsetY = 0
-            if (direction == "R")
-            {
+            if (direction == "R") {
                 offsetX = 1
-            } else if (direction == 'L')
-            {
+            } else if (direction == 'L') {
                 offsetX = -1
-            } else if (direction == 'U')
-            {
+            } else if (direction == 'U') {
                 offsetY = 1
             } else // D
             {
                 offsetY = -1
             }
 
-            for (i in 1..numSteps) {
+//            println "== $direction $numSteps =="
+
+            for (j in 1..numSteps) {
                 // Move H
-                headX += offsetX
-                headY += offsetY
+                def oldX = knots[0].x
+                def oldY = knots[0].y
+                knots[0].x += offsetX
+                knots[0].y += offsetY
 
-                // Move tail
-                if (Math.abs(headX - tailX) <= 1 && Math.abs(headY - tailY) <= 1) {
-                    // close enough; don't move T
-                } else {
-                    if (headX == tailX) {
-                        tailY += offsetY
-                    } else if (headY == tailY) {
-                        tailX += offsetX
-                    } else { // diagonal
-                        tailX += headX > tailX ? 1 : -1
-                        tailY += headY > tailY ? 1 : -1
+//                println "Moved H from [$oldX, $oldY] to [${knots[0].x}, ${knots[0].y}]"
+
+                // Move adjacent knots
+                for (i in 1..<numKnots) {
+                    def adjKnot = knots[i - 1]
+                    def currKnot = knots[i]
+
+                    oldX = currKnot.x
+                    oldY = currKnot.y
+
+                    if (Math.abs(adjKnot.x - currKnot.x) <= 1 && Math.abs(adjKnot.y - currKnot.y) <= 1) {
+                        // close enough; don't move
+                    } else {
+                        def closerX = adjKnot.x > currKnot.x ? 1 : -1
+                        def closerY = adjKnot.y > currKnot.y ? 1 : -1
+
+                        if (adjKnot.x == currKnot.x) {
+                            currKnot.y += closerY
+                        } else if (adjKnot.y == currKnot.y) {
+                            currKnot.x += closerX
+                        } else { // diagonal
+                            currKnot.x += closerX
+                            currKnot.y += closerY
+                        }
+//                        println "  Moved $i from [$oldX, $oldY] to [${currKnot.x}, ${currKnot.y}]"
                     }
-
-                    visitedCoords << new XY(tailX, tailY)
                 }
+
+                visitedCoords << new XY(knots[numKnots - 1])
             }
         })
 
@@ -60,12 +78,18 @@ class Day09 {
     }
 
     class XY {
-        final int x
-        final int y
+        int x
+        int y
 
         XY(int x, int y) {
             this.x = x
             this.y = y
+        }
+
+        XY(XY original)
+        {
+            x = original.x
+            y = original.y
         }
 
         boolean equals(o) {
--- src/test/groovy/aoc/Day09Test.groovy
+++ src/test/groovy/aoc/Day09Test.groovy
@@ -13,8 +13,9 @@ class Day09Test {
     @CsvSource([
             "09-control.txt,13,true",
             "09-data.txt,6044,true",
-//            "09-control.txt,8,false",
-//            "09-data.txt,671160,false",
+            "09-control.txt,1,false",
+            "09-control2.txt,36,false",
+            "09-data.txt,2384,false",
     ])
     void countVisitedCoordinates(String fileName, int result, boolean part1) {
         assertThat(new Day09().countVisitedCoordinates(fileName, part1)).isEqualTo(result)
